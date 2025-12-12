0f3cfd7091ca6549000d0a7eb13adb17a997ab71
jeongkijeong <81601886+jeongkijeong@users.noreply.github.com>
2025-12-12 17:10:27 +0900

    Update bard.py

--- src/model/bard.py
+++ src/model/bard.py
@@ -1,8 +1,8 @@
 import bardapi
 import os
 
-# os.environ['_BARD_API_KEY'] = 'XQhe5-0j5-0oh-b8SPqu8WUd39PtI0VERWfPWQN6f8-gtEdkWg2kDfHUz0p-rsDgkzN_tw.'
-os.environ['_BARD_API_KEY'] = 'Ywhe52ZAS1X07XH77klo3Tbi41e4bWebbo4v9Tlmzgeg7gMblVdRhdMkNV7ItwYmcgxhww.'
+
+os.environ['_BARD_API_KEY'] = ''
 
 
 def call(prompt):
